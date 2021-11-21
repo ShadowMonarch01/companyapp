e97f15a9d3b1af33f974532620e7c0dd05bbbb74
PrinceCodes <princecodes247@gmail.com>
2021-11-21 23:07:51 +0100

    feat: implement projects fetch

--- screens/projectdetails/AllProjects.js
+++ screens/projectdetails/AllProjects.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {View, Text,StyleSheet} from 'react-native';
 
 const AllProjectsScreen = ({navigation}) =>{
 
+     	const [projects, setProjects] = useState([])
+
+     	useEffect(()=> {
+
+	      fetch('https://rpyendapp.herokuapp.com/getprojects')
+	      .then((response) => response.json())
+	      .then((response) => {
+	         setProjects(response.data)
+	      })
+	      .catch((error) => {
+	          console.error(error);
+	      });
     
-    
+     	},[])
+
     return(
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
           <Text>All Projects</Text>
+          {projects.map(item => <Text>{item.name}</Text>)}
        </View>
     );
 }
