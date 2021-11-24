e03db6e690238089d9404b5c194b8caee9f176e2
PrinceCodes <princecodes247@gmail.com>
2021-11-24 10:42:08 +0100

    fix: import UseFocusEffect

--- screens/projectdetails/AllProjects.js
+++ screens/projectdetails/AllProjects.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect, useCallback} from 'react';
 import {View, Text,StyleSheet} from 'react-native';
-
+import { useFocusEffect } from "@react-navigation/native"
 // For OPTION 2
 // import { useIsFocused } from "@react-navigation/native"
 
